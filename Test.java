b965b4420e9de298a4a08f8fca93ca9c68275229
Sravanthi1108 <101916647+Sravanthi1108@users.noreply.github.com>
2022-05-12 18:49:25 +0530

    files added

---
+++ Test.java
@@ -0,0 +1,24 @@
+package hibernatepac;
+public class Test {
+	
+	int testId;//pk
+	String testName;
+	
+public Test()
+{
+	       
+}
+public int getTestId() {
+	return testId;
+}
+public void setTestId(int testId) {
+	this.testId = testId;
+}
+public String getTestName() {
+	return testName;
+}
+public void setTestName(String testName) {
+	this.testName = testName;
+}
+
+}
